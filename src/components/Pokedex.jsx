53e905f80652d9d3560a6c53d036be5154454b8e
Cristian-M-B <crisbaronetto@hotmail.com>
2021-11-24 18:33:13 -0300

    filtrado y paginado

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -1,44 +1,59 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Grid, FormControl, InputLabel, Select, MenuItem } from '@material-ui/core';
+import { Grid, FormControl, InputLabel, Select, MenuItem, Pagination } from '@material-ui/core';
 import PokeCard from './PokeCard';
-import { sortByName, sortByNumber } from '../redux/actions/index';
+import { sortByName, sortByNumber, filterByType } from '../redux/actions/index';
 
 export default function Pokedex() {
     const pokemons = useSelector(state => state.pokemons);
+    const types = useSelector(state => state.types);
     const dispatch = useDispatch();
-    const [render, setRender] = useState('');
     const [select, setSelect] = useState({
         name:'',
         number:'',
         filter:''
     });
 
+    const [page, setPage] = useState(1);
+    const [pokemonsPerPage, setPokemonsPerPage] = useState(12);
+    const indexLastPokemons = page * pokemonsPerPage;
+    const indexFirstPokemons = indexLastPokemons - pokemonsPerPage;
+    const currentPokemons = pokemons.slice(indexFirstPokemons, indexLastPokemons);
+
+    
+    function handleChange(event, value) {
+        setPage(value);
+    }
+
     function handleSortName(e){
         dispatch(sortByName(e.target.value));
-        setRender(`Sort name ${e.target.value}`);
         setSelect({
             ...select,
-            name: e.target.value
+            name: e.target.value,
+            number:''
         })
+        setPage(1);
     }
 
     function handleSortNumber(e){
         dispatch(sortByNumber(e.target.value));
-        setRender(`Sort number ${e.target.value}`);
         setSelect({
             ...select,
-            number: e.target.value
+            number: e.target.value,
+            name:''
         })
+        setPage(1);
     }
 
-    // function handleFilterType(e){
-    //     setRender(`Filter ${e.target.value}`);
-    //     setSelect({
-    //         ...select,
-    //         filter: e.target.value
-    //     })
-    // }
+    function handleFilterType(e){
+        dispatch(filterByType(e.target.value));
+        setSelect({
+            filter: e.target.value,
+            name:'',
+            number:''
+        })
+        setPage(1);
+    }
 
     return (
         <>
@@ -46,9 +61,8 @@ export default function Pokedex() {
                 container
                 direction='row'
                 justifyContent='center'
-                style={{marginTop:'5vh'}}
             >
-                <FormControl variant="standard" style={{width:120}}>
+                <FormControl variant="standard" style={{width:120, margin:'3vh'}}>
                     <InputLabel>Nombre</InputLabel>
                     <Select value={select.name} onChange={(e) => handleSortName(e)}>
                         <MenuItem value='A-Z'>A - Z</MenuItem>
@@ -56,31 +70,46 @@ export default function Pokedex() {
                     </Select>
                 </FormControl>
 
-                <FormControl variant="standard" style={{width:120}}>
+                <FormControl variant="standard" style={{width:120, margin:'3vh'}}>
                     <InputLabel>Número</InputLabel>
                     <Select value={select.number} onChange={(e) => handleSortNumber(e)}>
                         <MenuItem value='1-150'>1 - 150</MenuItem>
                         <MenuItem value='150-1'>150 - 1</MenuItem>
                     </Select>
                 </FormControl>
-                {/* <FormControl variant="standard" style={{width:120}}>
+                <FormControl variant="standard" style={{width:120, margin:'3vh'}}>
                     <InputLabel>Tipo</InputLabel>
-                    <Select value={select.filter} onChange={e => handleFilterType(e)}>
+                    <Select value={select.filter} onChange={(e) => handleFilterType(e)}>
                         <MenuItem value='all'>Todos</MenuItem>
-                        <MenuItem value=''></MenuItem>
+                        {types?.map(type => <MenuItem key={type} value={type}>{type}</MenuItem>)}
                     </Select>
-                </FormControl> */}
+                </FormControl>
             </Grid>
             <Grid
                 container
                 direction='row'
                 justifyContent='center'
             >
-                {pokemons?.map(poke => {
+                {currentPokemons?.map(poke => {
                     return <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
                         <PokeCard id={poke.id} name={poke.name} type={poke.types} image={poke.image} key={poke.id} />
                     </Grid>
                 })}
+            </Grid>
+            <Grid
+                container
+                direction='row'
+                justifyContent='center'
+            >
+                <Pagination
+                    count={Math.ceil(pokemons.length / pokemonsPerPage)}
+                    page={page}
+                    onChange={handleChange}
+                    variant="outlined"
+                    shape="rounded"
+                    color="primary"
+                    style={{ marginBottom: "2vw ", marginTop: "1vw" }}
+                />
             </Grid>
         </>
     )
