6b7a5d7f7767b5de97965f3adb3620e97f9dbf30
Cristian-M-B <crisbaronetto@hotmail.com>
2021-11-25 18:25:14 -0300

    favorites

--- src/redux/actions/constants.js
+++ src/redux/actions/constants.js
@@ -5,5 +5,7 @@ export const REMOVE_DETAIL = 'REMOVE_DETAIL';
 export const SORT_BY_NAME = 'SORT_BY_NAME';
 export const SORT_BY_NUMBER = 'SORT_BY_NUMBER';
 export const FILTER_BY_TYPE = 'FILTER_BY_TYPE';
+export const ADD_FAVORITE = 'ADD_FAVORITE';
+export const REMOVE_FAVORITE = 'REMOVE_FAVORITE';
 
 
