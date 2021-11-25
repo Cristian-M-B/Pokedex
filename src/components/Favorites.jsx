925459c6c998b10ee59ff9ffdc855817b1141881
Cristian-M-B <crisbaronetto@hotmail.com>
2021-11-25 18:23:25 -0300

    componente favorites

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Grid } from '@material-ui/core';
+import PokeCard from './PokeCard';
 
 export default function Favorites() {
+    const favorites = useSelector(state => state.favorites);
     return (
-        <div>
-            
-        </div>
+        <>
+            {favorites?.map(poke => {
+                return <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
+                    <PokeCard id={poke.id} name={poke.name} type={poke.types} image={poke.image} key={poke.id} />
+                </Grid>
+            })}
+        </>
     )
 }
 
