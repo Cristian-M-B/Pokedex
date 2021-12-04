9203180dc8909d6c006e563c08c541224e85985f
Cristian-M-B <crisbaronetto@hotmail.com>
2021-12-03 22:21:36 -0300

    spinner

--- src/components/PokeDetail.jsx
+++ src/components/PokeDetail.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getDetail, removeDetail } from '../redux/actions/index';
-import { Grid, CardMedia, Typography } from '@material-ui/core';
+import { Grid, CardMedia, Typography, CircularProgress } from '@material-ui/core';
 
 export default function PokeDetail() {
     const {id} = useParams();
@@ -18,32 +18,42 @@ export default function PokeDetail() {
     const pokemon = useSelector(state => state.detail);
 
     return (
-        <Grid 
-            container
-            direction= 'row'
-            justifyContent= 'center'
-            style={{marginTop:'20vh'}}
-        >
-            <Grid>
-                <CardMedia
-                    component="img"
-                    height="300vh"
-                    image={pokemon?.image}
+        <>
+            {pokemon.name ?
+                <Grid
+                    container
+                    direction='row'
+                    justifyContent='center'
+                    style={{ marginTop: '20vh' }}
+                >
+                    <Grid>
+                        <CardMedia
+                            component="img"
+                            height="300vh"
+                            image={pokemon?.image}
+                        />
+                    </Grid>
+                    <Grid style={{ marginLeft: '15vh' }}>
+                        <Typography align='left' variant='h4' style={{ marginBottom: '4vh' }}> #{pokemon?.id} {pokemon?.name}</Typography>
+                        <Grid container justifyContent='space-around'>
+                            {pokemon?.types?.map(type => {
+                                return <Typography key={type} style={{ display: 'inline-block' }}>{type}</Typography>
+                            })}
+                        </Grid>
+                        <Typography align='left' style={{ marginTop: '4vh' }}>HP: {pokemon?.hp}</Typography>
+                        <Typography align='left'>ATTACK: {pokemon?.attack}</Typography>
+                        <Typography align='left'>DEFENCE: {pokemon?.defense}</Typography>
+                        <Typography align='left'>SPEED: {pokemon?.speed}</Typography>
+                        <Typography align='left'>HEIGHT: {pokemon?.height}</Typography>
+                        <Typography align='left'>WEIGHT: {pokemon?.weight}</Typography>
+                    </Grid>
+                </Grid>
+                : <CircularProgress
+                    size={60}
+                    style={{ marginTop: '40vh' }}
                 />
-            </Grid>
-            <Grid style={{marginLeft:'15vh'}}>
-                <Typography align='left' variant='h4' style={{marginBottom:'4vh'}}>{pokemon?.name}</Typography>
-                {pokemon?.types?.map(p => {
-                    return <Typography key={p} align='left'>{p}</Typography>
-                })}
-                <Typography align='left' style={{marginTop:'4vh'}}>HP: {pokemon?.hp}</Typography>
-                <Typography align='left'>ATTACK: {pokemon?.attack}</Typography>
-                <Typography align='left'>DEFENCE: {pokemon?.defense}</Typography>
-                <Typography align='left'>SPEED: {pokemon?.speed}</Typography>
-                <Typography align='left'>HEIGHT: {pokemon?.height}</Typography>
-                <Typography align='left'>WEIGHT: {pokemon?.weight}</Typography>
-            </Grid>
-        </Grid>
+            }
+        </>
     )
 }
 
