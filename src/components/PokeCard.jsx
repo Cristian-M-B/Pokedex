a3e34452662d1a507f3821c342b29170d6a0cffa
Cristian-M-B <crisbaronetto@hotmail.com>
2021-12-06 22:40:15 -0300

    share pokemon

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { Card, CardMedia , CardContent , CardActions , Typography, IconButton, Divider } from '@material-ui/core';
-import { Favorite } from '@material-ui/icons';
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton, Divider } from '@material-ui/core';
+import { Favorite, Share } from '@material-ui/icons';
+import { WhatsappShareButton, FacebookShareButton, WhatsappIcon, FacebookIcon } from 'react-share';
 import { addFavorite, removeFavorite } from '../redux/actions/index';
 
 export default function PokeCard({id, name, image, types}) {
+    const [shareOpen, setShareOpen] = useState(false);
     const favorites = useSelector(state => state.favorites)
     const dispatch = useDispatch();
     let status = false;
@@ -17,6 +19,10 @@ export default function PokeCard({id, name, image, types}) {
         } else {
             dispatch(removeFavorite(id));
         }
+    }
+
+    function handleShare(){
+        setShareOpen(!shareOpen);
     }
 
     return (
@@ -37,12 +43,32 @@ export default function PokeCard({id, name, image, types}) {
             {favorites?.forEach(pokemon => {
                 if(pokemon.id === id) status = true 
             })}
-            <CardActions>
+            <CardActions style={{display:'flex', justifyContent:'space-between'}}>
                 <IconButton size="small" onClick={handleFavorite}>
                     {status ? 
                         <Favorite color='error'/>
                         : <Favorite/>
                     }
+                </IconButton>
+                {shareOpen && (
+                    <>
+                        <WhatsappShareButton
+                            url={`http://localhost:3000/pokedex/detail/${id}`}
+                            title="¡Mira este pokemon!"
+                        >
+                            <WhatsappIcon round={true} style={{ width: "5vh", height: "5vh" }} />
+                        </WhatsappShareButton>
+                        <FacebookShareButton
+                            url={`https://amadeus.vercel.app/detail/614df55e887587966406cd59`}
+                            quote="¡Mira este pokemon!"
+                            hashtag="#pokemon"
+                        >
+                            <FacebookIcon round={true} style={{ width: "5vh", height: "5vh" }} />
+                        </FacebookShareButton>
+                    </>
+                )}
+                <IconButton size="small" onClick={handleShare}>
+                    <Share/>
                 </IconButton>
             </CardActions>
         </Card >
