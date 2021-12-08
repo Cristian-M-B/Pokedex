e6a306d26377125802c44437db25c6946051a815
Cristian-M-B <crisbaronetto@hotmail.com>
2021-12-07 22:22:46 -0300

    correction

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -4,6 +4,7 @@ import { Grid, FormControl, InputLabel, Select, MenuItem, Pagination, CircularPr
 import Nav from './Nav';
 import PokeCard from './PokeCard';
 import { sortByName, sortByNumber, filterByType } from '../redux/actions/index';
+// import images from '../assets/images.js';
 
 export default function Pokedex() {
     const pokemons = useSelector(state => state.pokemons);
@@ -85,10 +86,24 @@ export default function Pokedex() {
                         <FormControl variant="standard" style={{ width: 120, margin: '3vh' }}>
                             <InputLabel>Tipo</InputLabel>
                             <Select value={select.filter} onChange={(e) => handleFilterType(e)}>
-                                <MenuItem value='all'>Todos</MenuItem>
+                                <MenuItem value='all'>all</MenuItem>
                                 {types?.map(type => <MenuItem key={type} value={type}>{type}</MenuItem>)}
                             </Select>
                         </FormControl>
+
+                        {/* <FormControl variant="standard" style={{ width: 120, margin: '3vh' }}>
+                            <InputLabel>Tipo</InputLabel>
+                            <Select value={select.filter} onChange={(e) => handleFilterType(e)}>
+                                <MenuItem value='all'>all</MenuItem>
+                                {types?.map(type => <MenuItem key={type} value={type}>
+                                    <CardMedia
+                                        component="img"
+                                        height='70vh'
+                                        image={images[type]}
+                                    />
+                                </MenuItem>)}
+                            </Select>
+                        </FormControl> */}
                     </Grid>
 
                     <Grid
@@ -98,7 +113,7 @@ export default function Pokedex() {
                     >
                         {currentPokemons?.map(poke => {
                             return <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
-                                <PokeCard id={poke.id} name={poke.name} type={poke.types} image={poke.image} key={poke.id} />
+                                <PokeCard id={poke.id} name={poke.name} types={poke.types} image={poke.image} key={poke.id} />
                             </Grid>
                         })}
                     </Grid>
