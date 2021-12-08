24d34d428d3e52f9d9941c94fc3bb12f6339c782
Cristian-M-B <crisbaronetto@hotmail.com>
2021-12-07 22:09:32 -0300

    images

---
+++ src/assets/images.js
@@ -0,0 +1,39 @@
+import bug from './images/bug.jpg';
+import dark from './images/dark.jpg';
+import dragon from './images/dragon.jpg';
+import electric from './images/electric.jpg';
+import fairy from './images/fairy.jpg';
+import fighting from './images/fighting.jpg';
+import fire from './images/fire.jpg';
+import flying from './images/flying.jpg';
+import ghost from './images/ghost.jpg';
+import grass from './images/grass.jpg';
+import ground from './images/ground.jpg';
+import ice from './images/ice.jpg';
+import normal from './images/normal.jpg';
+import poison from './images/poison.jpg';
+import psychic from './images/psychic.jpg';
+import rock from './images/rock.jpg';
+import steel from './images/steel.jpg';
+import water from './images/water.jpg';
+
+export default {
+    bug,
+    dark,
+    dragon,
+    electric,
+    fairy,
+    fighting,
+    fire,
+    flying,
+    ghost,
+    grass,
+    ground,
+    ice,
+    normal,
+    poison,
+    psychic,
+    rock,
+    steel,
+    water
+}
