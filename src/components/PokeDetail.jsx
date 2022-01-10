075fee7c1e657e357fc70b131094e8b5416caa6d
Cristian-M-B <crisbaronetto@hotmail.com>
2022-01-10 20:29:14 -0300

    add footer

--- src/components/PokeDetail.jsx
+++ src/components/PokeDetail.jsx
@@ -4,11 +4,12 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getDetail, removeDetail } from '../redux/actions/index';
 import { Grid, CardMedia, Typography, CircularProgress } from '@material-ui/core';
 import Nav from './Nav';
+import Footer from './Footer';
 import images from '../assets/images.js';
 import './animations.css';
 
 export default function PokeDetail() {
-    const {id} = useParams();
+    const { id } = useParams();
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -16,7 +17,7 @@ export default function PokeDetail() {
         return () => {
             dispatch(removeDetail())
         }
-    },[])
+    }, [])
 
     const pokemon = useSelector(state => state.detail);
 
@@ -29,36 +30,44 @@ export default function PokeDetail() {
                     direction='row'
                     justifyContent='center'
                     alignItems='center'
-                    style={{ marginTop: '20vh' }}
+                    style={{ minHeight: '84vh' }}
                     className='animations'
                 >
-                    <Grid justifyContent='center'>
+                    <Grid justifyContent='center' style={{ margin: '2vh' }}>
                         <CardMedia
                             component="img"
                             height="300vh"
                             image={pokemon?.image}
                         />
                     </Grid>
-                    <Grid justifyContent='center' style={{ marginLeft: '15vh' }}>
+                    <Grid justifyContent='center' style={{ margin: '2vh' }}>
                         <Typography align='left' variant='h4' style={{ marginBottom: '4vh' }}> #{pokemon?.id} {pokemon?.name}</Typography>
                         <Grid container direction='row' justifyContent='center'>
                             {pokemon?.types?.map(type => {
                                 return <img key={type} src={images[type]} height='90vh' />
                             })}
                         </Grid>
-                        <Typography align='left' style={{ marginTop: '4vh' }}>HP: {pokemon?.hp}</Typography>
-                        <Typography align='left'>ATTACK: {pokemon?.attack}</Typography>
-                        <Typography align='left'>DEFENCE: {pokemon?.defense}</Typography>
-                        <Typography align='left'>SPEED: {pokemon?.speed}</Typography>
-                        <Typography align='left'>HEIGHT: {pokemon?.height}</Typography>
-                        <Typography align='left'>WEIGHT: {pokemon?.weight}</Typography>
+                        <Grid container direction='row' justifyContent='space-between' style={{ marginTop: '4vh' }}>
+                            <Typography>HP: {pokemon?.hp}</Typography>
+                            <Typography>SPEED: {pokemon?.speed}</Typography>
+                        </Grid>
+                        <Grid container direction='row' justifyContent='space-between'>
+                            <Typography style={{ marginRight: '2vh' }}>ATTACK: {pokemon?.attack}</Typography>
+                            <Typography>DEFENCE: {pokemon?.defense}</Typography>
+                        </Grid>
+                        <Grid container direction='row' justifyContent='space-between'>
+                            <Typography>HEIGHT: {pokemon?.height}</Typography>
+                            <Typography>WEIGHT: {pokemon?.weight}</Typography>
+                        </Grid>
                     </Grid>
                 </Grid>
-                : <CircularProgress
-                    size={60}
-                    style={{ marginTop: '40vh' }}
-                />
+                : <div style={{ height: '84vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                    <CircularProgress
+                        size={60}
+                    />
+                </div>
             }
+            <Footer />
         </>
     )
 }
