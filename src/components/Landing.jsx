c1cce0772f4ab02f2b4bb394db2e335a4bcd3dc3
Cristian-M-B <crisbaronetto@hotmail.com>
2022-01-26 15:24:42 -0300

    styles

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from '@material-ui/core';
 import '../styles/landing.css';
+import '../styles/animations.css';
 
 export default function Landing() {
     return (
         <div className='landing'>
             <Link to={"/pokedex"} style={{ textDecoration: 'none' }}>
-                <Button variant="contained" className='button'>
+                <Button variant="contained" className='button animations-inverse'>
                     Pokedex
                 </Button>
             </Link>
